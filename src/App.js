2680e7e7f83019b251b5ac51e94dd762ea3f7af4
oreoA09 <oreo.akanni@gmail.com>
2022-10-17 18:16:11 +0000

    addColor function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
 import './App.css';
 
 function App() {
   let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
 
-  return (
+  let colorMap = colors.map((color, i)=>{
+    return (
+        <ColorBlock key={i} color={color}/>
+    );
+  })
+
+  const checkColor = (newColor) =>{
+    const cool = new Option().style;
+    cool.color = newColor;
+    return cool.color !== ""
+  }
+
+  const addColor = (newColor) => {
+    if(checkColor(newColor)){
+    setColors([...colors])
+    }
+  }
+
+  return(
     <div className="App">
-        Colors
+      <ColorForm addColor={addColor}/>
+      <div className='colorCards'>
+      {colorMap}
+    </div>
     </div>
   );
 }
---
+++ src/ColorForm.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ColorBlock from './ColorBlock'
+
+function ColorForm(props){
+
+    let [input, setInput] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        e.target[0].value = "";
+        props.addColor(input)
+    }
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={(e)=>setInput(e.target.value)} placeholder="Change my color"/>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default ColorForm
